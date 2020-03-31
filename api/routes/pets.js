3abf8fe266d1ec2b7aefe20e4ae3d7a2ccf34442
Cassie Schwendiman <cassie.schwendiman@gmail.com>
2020-03-31 15:48:39 -0500

    add basic sorting to pets table

--- api/routes/pets.js
+++ api/routes/pets.js
@@ -6,7 +6,23 @@ var pets = require('../data/denormalizedPets.json');
 
 /* GET all pets */
 router.get('/', function(req, res, next) {
-  res.json(pets);
+  const column = req.query.sort
+  const ascending = req.query.ascending
+  // This is awful, do all of this with sql
+  if (column && ascending) {
+    res.json(pets.sort((a, b) => {
+      if (a[column] === b[column]) {
+        return 0
+      }
+      if (ascending === 'true') {
+        return a[column] < b[column] ? -1 : 1
+      } else {
+        return a[column] > b[column] ? -1 : 1
+      }
+    }))
+  } else {
+    res.json(pets);    
+  }
 });
 
 /* GET pet by petId */
--- app/src/components/pets/pets.js
+++ app/src/components/pets/pets.js
@@ -2,21 +2,35 @@ import React, { useState, useEffect } from 'react'
 
 function Pets({ setSelectedPet }) {
     const [ pets, setPets ] = useState([])
+    const [ sort, setSort ] = useState({
+        column: 'id',
+        ascending: true
+    })
 
     useEffect(() => {
-        fetch("/pets/")
+        fetch(`/pets?sort=${sort.column}&ascending=${sort.ascending}`)
         .then(res => res.json())
         .then(setPets)
-    }, [])
-
+    }, [sort])
+    
+    function updateSort(column) {
+        if (sort.column === column) {
+            setSort({ ...sort, ascending: !sort.ascending})
+        } else {
+            setSort({
+                column,
+                ascending: true
+            })
+        }
+    }
 
     return (
         <div className={'pets'}>
             <table>
                 <thead>
                     <tr>
-                        <th>Pet ID</th>
-                        <th>Pet Name</th>
+                        <th><button onClick={updateSort.bind(this, 'id')}>Pet ID</button></th>
+                        <th><button onClick={updateSort.bind(this, 'name')}>Pet Name</button></th>
                     </tr>
                 </thead>
                 <tbody>
